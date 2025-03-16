3f7b988960e55aeadc6c9c9ff913cb54c97d765a
LelouchelawlietLIEBERT <154049616+LelouchelawlietLIEBERT@users.noreply.github.com>
2025-03-16 14:34:12 +0530

    Update page.tsx

--- app/AddTopic/page.tsx
+++ app/AddTopic/page.tsx
@@ -21,7 +21,7 @@ export default function AddTopic() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/api/topics", {
+      const res = await fetch("https://ghost15.vercel.app/api/topics", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
